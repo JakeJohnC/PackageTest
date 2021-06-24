d6781699070870ff82c2f5b0f8cbfaeac77f2a92
JakeJohnC <62907152+JakeJohnC@users.noreply.github.com>
2021-06-24 10:50:13 +0200

    Add files via upload

---
+++ Runtime/PackageA/PackageAClass.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageAClass
+{
+	public static void NowYouSeeMe()
+	{
+		Debug.Log("A");
+	}
+}
---
+++ Runtime/PackageB/PackageBClass.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageBClass
+{
+	public static void NowYouSeeMe()
+	{
+		Debug.Log("B");
+	}
+}
